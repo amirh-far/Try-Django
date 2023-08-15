842ab7375e60e11c6be0e5c8aa7ebc6bcf24f63f
amirh-far <Ahosein54@gamil.com>
2023-08-15 16:18:44 +0330

    16 adding data to the database & puting it in homepage

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -29,6 +29,9 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+# python manage.py makemigrations 
+# python manage.py migrate
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'articles',
 ]
 
 MIDDLEWARE = [
--- trydjango/views.py
+++ trydjango/views.py
@@ -2,13 +2,20 @@
 To render html web pages
 """
 from django.http import HttpResponse
+from articles.models import Article
 
-HTML_STRING = """
-<h1> Hello World </h1>
+article_obj = Article.objects.get(id=1)
+
+HTML_STRING = f"""
+<h1> {article_obj.title} id: {article_obj.id}</h1>
+
+<p1> {article_obj.content} </p1>
 
 """
 
-def home_view(request):
+
+
+def home_view (request):
     """
     Take in a request (Django sends request)
     Return Html as a response (We picj to return the response)
