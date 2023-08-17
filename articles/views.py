1099c7c7853dad58509d0260df69b6ab1495100b
amirh-far <Ahosein54@gamil.com>
2023-08-17 16:03:30 +0330

    Basic HTML Form in Django to write new data to Database

--- articles/views.py
+++ articles/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import render
 
 def article_search_view(request):
     query_dict = request.GET
+    # print(dir(request))
     query = query_dict.get("q")
 
     article_obj = None
@@ -18,14 +19,14 @@ def article_search_view(request):
     context = {"object": article_obj}
     return render(request,"articles/search.html", context=context)
 
-def article_home_view(request):
+# def article_home_view(request):
 
 
-    HTML_RESPONSE = ""
+#     HTML_RESPONSE = ""
 
 
 
-    return HttpResponse(HTML_RESPONSE)
+#     return HttpResponse(HTML_RESPONSE)
 
 def article_detail_view(request, id, *args, **argv):
     article_obj = None
@@ -37,6 +38,22 @@ def article_detail_view(request, id, *args, **argv):
     }
 
     return render(request, "articles/detail.html", context=context)
+
+
+def article_create_view(request, *args, **argv):
+    context = {"created" : False}
+    if request.method == "POST":
+        # Or can use the request.GET to get the values you need but you need to use the get method instead
+        article_obj = Article()
+        article_obj.title = request.POST.get("title")
+        article_obj.content = request.POST.get("content")
+        article_obj.save()
+        # or you can do this line of code instead: Article.objects.create(title=title, content=content)
+        context["object"] = article_obj
+        context["created"] = True
+    # print(request.GET)
+
+    return render(request, "articles/create.html", context=context)
 
     
 
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -17,14 +17,16 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import home_view
-from articles.views import article_home_view
+# from articles.views import article_home_view
 from articles.views import article_detail_view
 from articles.views import article_search_view
+from articles.views import article_create_view
 
 urlpatterns = [
     path('', home_view), # index /home /root
     # path('articles/', article_home_view),
     path('articles/', article_search_view),
-    path('articles/<int:id>', article_detail_view),
+    path('articles/<int:id>/', article_detail_view),
+    path("articles/create/", article_create_view),
     path('admin/', admin.site.urls)
 ]
