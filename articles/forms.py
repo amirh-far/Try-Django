ce699ddfbd6db2d6eb1468bf9f6861ccfb7efbb2
amirh-far <Ahosein54@gamil.com>
2023-08-19 14:47:51 +0330

    27 add clean method and FieldError and NoneField Error

--- articles/forms.py
+++ articles/forms.py
@@ -7,7 +7,24 @@ class ArticleForm(forms.Form):
     def clean_title(self):
         cleaned_data = self.cleaned_data # Dictionary
         print(cleaned_data)
-        cleaned_title = cleaned_data.get("title")
-        if cleaned_title.strip().lower() == "yo":
-            raise forms.ValidationError("This title is already taken")
-        return cleaned_title
+        title = cleaned_data.get("title")
+        # if title.strip().lower() == "yo":
+        #     # Field Error:
+        #     self.add_error("title", "this title is used")
+            # Non Field Error:
+            # raise forms.ValidationError("This title is already taken")
+        return title
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        print("All data:", cleaned_data)
+        title = cleaned_data.get("title")
+        content = cleaned_data.get("content")
+        if title.strip().lower() == "yo":
+            self.add_error("title", "this title is used")
+            # raise forms.ValidationError("This title is already taken")
+        if title.strip().lower() == "yo" or content.strip().lower() == "yo":
+            #Field Error:
+            self.add_error("content", "Yo can not be in content. already used")
+            #Non Field Error:
+            raise forms.ValidationError("Title or content already taken")
+        return cleaned_data
