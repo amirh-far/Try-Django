ad22cf9a4b84ddda0f3daaffd3a2fe01836d7bb4
amirh-far <Ahosein54@gamil.com>
2023-10-10 18:09:50 +0330

    88-2 some bug fixes

--- meals/models.py
+++ meals/models.py
@@ -1,51 +1,66 @@
-from django.db import models
 from django.conf import settings
-from django.urls import reverse
-
+from django.db import models
+from django.db.models.signals import post_save
 from recipes.models import Recipe
 
 User = settings.AUTH_USER_MODEL
 
 class MealStatus(models.TextChoices):
-    PENDING = "p", "Pending"
-    COMPLETED = "c", "Completed"
-    EXPIRED = "e", "Expired"
-    ABORTED = "a", "Aborted"
+    PENDING = 'p', 'Pending'
+    COMPLETED = 'c', 'Completed'
+    EXPIRED = 'e', 'Expired'
+    ABORTED = 'a', 'Aborted'
 
-# MEAL_CHOICES = 
+
 
 class MealQuerySet(models.QuerySet):
     def by_user_id(self, user_id):
         return self.filter(user_id=user_id)
     
-    def by_user_id(self, user):
+    def by_user(self, user):
         return self.filter(user=user)
     
     def pending(self):
         return self.filter(status=MealStatus.PENDING)
-    
+
     def completed(self):
         return self.filter(status=MealStatus.COMPLETED)
-    
-    def expired(self):
-        return self.filter(status=MealStatus.EXPIRED)
-    
+        
     def aborted(self):
         return self.filter(status=MealStatus.ABORTED)
-    
+        
+    def expired(self):
+        return self.filter(status=MealStatus.EXPIRED)
+
     def in_queue(self, recipe_id):
         return self.pending().filter(recipe_id=recipe_id).exists()
-    
+
+   
 class MealManager(models.Manager):
     def get_queryset(self):
-            return MealQuerySet(self.model, using=self._db)
+        return MealQuerySet(self.model, using=self._db)
+
+    def by_user_id(self, user_id):
+        return self.get_queryset().by_user_id(user_id)
     
+    def by_user(self, user):
+        return self.get_queryset().by_user(user)
+
+    def get_queue(self, user, prefetch_ingredients=False):
+        qs = self.get_queryset().by_user(user).pending()
+        if prefetch_ingredients:
+            return qs.prefetch_related('recipe__recipeingredient')
+        return qs
+
     def toggle_in_queue(self, user_id, recipe_id):
         qs = self.get_queryset().all().by_user_id(user_id)
         already_queued = qs.in_queue(recipe_id)
+        added = None
         if already_queued:
             recipe_qs = qs.filter(recipe_id=recipe_id)
-            recipe_qs.update(status=MealStatus.ABORTED)
+            for instance in recipe_qs:
+                instance.status = MealStatus.ABORTED
+                instance.save()
             added = False
         else:
             obj = self.model(
@@ -57,13 +72,11 @@ class MealManager(models.Manager):
             added = True
         return added
 
-
 class Meal(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True) 
+    updated = models.DateTimeField(auto_now=True) 
     status = models.CharField(max_length=1, choices=MealStatus.choices, default=MealStatus.PENDING)
 
     objects = MealManager()
-
--- meals/views.py
+++ meals/views.py
@@ -1,30 +1,33 @@
-from django.shortcuts import render, HttpResponse
-from django.http import  HttpResponseBadRequest
-from .models import Meal
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import render
+
 from recipes.models import Recipe
+from .models import Meal
+
 def meal_queue_toggle_view(request, recipe_id=None):
     if not request.htmx:
         return HttpResponseBadRequest()
     user = request.user
     user_id = None
-    if user.is_authenticated:
-        user_id = user.id
-    if user_id is None:
+    if not user.is_authenticated:
         return HttpResponse("Must be logged in", status=400)
+    user_id = user.id
+    if user_id is None:
+        return 
     if request.method == "POST":
         is_valid_recipe = False
         try:
-           recipe_obj = Recipe.objects.get(recipe_id, user=user) 
-           is_valid_recipe = True
+            recipe_obj = Recipe.objects.get(user=user, id=recipe_id)
+            is_valid_recipe = True
         except:
-           pass
+            pass
         if is_valid_recipe:
-           Meal.objects.toggle_in_queue(user_id, recipe_id)
-    is_pending = Meal.objects.all().by_user_id(user_id).in_queue(recipe_id)
+            Meal.objects.toggle_in_queue(user_id, recipe_id)
+    is_pending = Meal.objects.by_user_id(user_id).in_queue(recipe_id)
     toggle_label = "Add to meals" if not is_pending else "Remove from meals"
-    context = {"recipe_id": recipe_id,
-               "toggle_label": toggle_label,
-               "is_pending": is_pending,
-               
+    context = {
+        'recipe_id': recipe_id, 
+        'toggle_label': toggle_label,
+        'is_pending': is_pending
     }
     return render(request, "meals/partials/queue-toggle.html", context)
